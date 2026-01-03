caa09a68476593dc8e98262f3012d6958acb1c77
santhiya@22 <santhiyasri22k@gmail.com>
2026-01-03 09:47:56 +0530

    bar and qr code   update

--- database/sync.ts
+++ database/sync.ts
@@ -12,7 +12,7 @@ import { syncTripsToServer } from "./local/triplog";
 // Replace with your LOCAL IP when testing on real device
 // Example: "http://192.168.1.5:5000/v1/sync"
 // ----------------------------------------------------
-const API_URL = "http://10.0.2.2:5000/v1/sync"; // https://fielduo.com/v1/sync
+const API_URL = "https://fielduo.com/v1/sync"; // https://fielduo.com/v1/sync
 
 // ----------------------------------------------------
 //  MAIN SYNC FUNCTION
